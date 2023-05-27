409700ac591ba348ce9a62fda783710e76a06318
simoneriillo <riillosimone@gmail.com>
2023-05-27 15:55:11 +0200

    primo commit

---
+++ src/main/java/it/prova/triage_be/TriageBeApplication.java
@@ -0,0 +1,13 @@
+package it.prova.triage_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriageBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriageBeApplication.class, args);
+	}
+
+}
